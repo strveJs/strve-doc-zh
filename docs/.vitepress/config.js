73ead85f447be36fd1129666e9f10433c9cd0b60
maomin <maomin@jiuheyun.com>
2023-05-31 14:00:57 +0800

    v5.1.1

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,146 +1,142 @@
 export default {
-	base: '/strve-doc-zh',
-	head: [['link', { rel: 'icon', href: '/strve-doc-zh/' + 'logo.png' }]],
-	markdown: {
-		theme: 'material-default',
-		lineNumbers: false,
-	},
-	locales: {
-		'/': {
-			lang: 'zh-CN',
-			title: 'Strve.js',
-			titleTemplate: '一个可以将字符串转换为视图的JS库',
-			description: '一个可以将字符串转换为视图的JS库',
-		},
-	},
-	themeConfig: {
-		logo: '/logo.png',
-		// algolia: {
-		// 	apiKey: 'bfcdc868c58895106c405a8d1a765c09',
-		// 	indexName: 'strvejs',
-		// 	appId: 'OO7L0TPJ7C',
-		// },
-		socialLinks: [
-			{ icon: 'twitter', link: 'https://twitter.com/maomincoding' },
-			{ icon: 'github', link: 'https://github.com/maomincoding/strve' },
-		],
-		locales: {
-			'/': {
-				nav: [
-					{
-						text: '演练场',
-						link: '/playground/',
-					},
-					{
-						text: '更新日志',
-						link: '/changeLog/',
-					},
-					{
-						text: '选择语言',
-						items: [
-							{
-								text: 'English',
-								link: 'https://maomincoding.github.io/strve-doc/',
-							},
-							{
-								text: '简体中文',
-								link: 'https://maomincoding.github.io/strve-doc-zh/',
-							},
-						],
-					},
-				],
-				outlineTitle: '此页',
-				sidebar: [
-					{
-						text: '指导',
-						collapsible: false,
-						items: [
-							{
-								text: '介绍',
-								link: '/guide/introduce/',
-							},
-							{
-								text: '开始',
-								link: '/guide/started/',
-							},
-							{
-								text: '安装',
-								link: '/guide/install/',
-							},
-						],
-					},
-					{
-						text: '要点',
-						collapsible: false,
-						items: [
-							{
-								text: 'API',
-								link: '/essentials/api/',
-							},
-							{
-								text: '用法',
-								link: '/essentials/usage/',
-							},
-						],
-					},
-					{
-						text: '工具',
-						collapsible: false,
-						items: [
-							{
-								text: 'CreateStrveApp',
-								link: '/tool/createStrveApp/',
-							},
-							{
-								text: 'StrveRouter',
-								link: '/tool/strveRouter/',
-							},
-							{
-								text: 'BabelPluginStrve',
-								link: '/tool/babelPluginStrve/',
-							},
-							{
-								text: 'VitePluginStrve',
-								link: '/tool/vitePluginStrve/',
-							},
-						],
-					},
-					{
-						text: '其他',
-						collapsible: false,
-						items: [
-							{
-								text: 'SFC',
-								link: '/other/sfc/',
-							},
-							{
-								text: '适配',
-								link: '/other/adapt/',
-							},
-							{
-								text: 'IDE 支持',
-								link: '/other/ide/',
-							},
-							{
-								text: 'UI 框架',
-								link: '/other/ui/',
-							},
-							{
-								text: '浏览器兼容性',
-								link: '/other/browser/',
-							},
-							{
-								text: '关于',
-								link: '/other/about/',
-							},
-						],
-					},
-				],
-			},
-		},
-		footer: {
-			message: 'Released under the MIT License.',
-			copyright: `Copyright © 2021-${new Date().getFullYear()} maomincoding`,
-		},
-	},
+  base: "/strve-doc-zh",
+  head: [["link", { rel: "icon", href: "/strve-doc-zh/" + "logo.png" }]],
+  markdown: {
+    theme: "material-default",
+    lineNumbers: false,
+  },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "Strve.js",
+      titleTemplate: "一个可以将字符串转换为视图的JS库",
+      description: "一个可以将字符串转换为视图的JS库",
+    },
+  },
+  themeConfig: {
+    logo: "/logo.png",
+    // algolia: {
+    // 	apiKey: 'bfcdc868c58895106c405a8d1a765c09',
+    // 	indexName: 'strvejs',
+    // 	appId: 'OO7L0TPJ7C',
+    // },
+    socialLinks: [
+      { icon: "twitter", link: "https://twitter.com/maomincoding" },
+      { icon: "github", link: "https://github.com/maomincoding/strve" },
+    ],
+    locales: {
+      "/": {
+        nav: [
+          {
+            text: "演练场",
+            link: "/playground/",
+          },
+          {
+            text: "更新日志",
+            link: "/changeLog/",
+          },
+          {
+            text: "选择语言",
+            items: [
+              {
+                text: "English",
+                link: "https://maomincoding.github.io/strve-doc/",
+              },
+              {
+                text: "简体中文",
+                link: "https://maomincoding.github.io/strve-doc-zh/",
+              },
+            ],
+          },
+        ],
+        outlineTitle: "此页",
+        sidebar: [
+          {
+            text: "指导",
+            collapsible: false,
+            items: [
+              {
+                text: "开始",
+                link: "/guide/started/",
+              },
+              {
+                text: "安装",
+                link: "/guide/install/",
+              },
+            ],
+          },
+          {
+            text: "要点",
+            collapsible: false,
+            items: [
+              {
+                text: "API",
+                link: "/essentials/api/",
+              },
+              {
+                text: "用法",
+                link: "/essentials/usage/",
+              },
+            ],
+          },
+          {
+            text: "工具",
+            collapsible: false,
+            items: [
+              {
+                text: "CreateStrveApp",
+                link: "/tool/createStrveApp/",
+              },
+              {
+                text: "StrveRouter",
+                link: "/tool/strveRouter/",
+              },
+              {
+                text: "BabelPluginStrve",
+                link: "/tool/babelPluginStrve/",
+              },
+              {
+                text: "VitePluginStrve",
+                link: "/tool/vitePluginStrve/",
+              },
+            ],
+          },
+          {
+            text: "其他",
+            collapsible: false,
+            items: [
+              {
+                text: "SFC",
+                link: "/other/sfc/",
+              },
+              {
+                text: "适配",
+                link: "/other/adapt/",
+              },
+              {
+                text: "IDE 支持",
+                link: "/other/ide/",
+              },
+              {
+                text: "UI 框架",
+                link: "/other/ui/",
+              },
+              {
+                text: "浏览器兼容性",
+                link: "/other/browser/",
+              },
+              {
+                text: "关于",
+                link: "/other/about/",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: `Copyright © 2021-${new Date().getFullYear()} maomincoding`,
+    },
+  },
 };
--- docs/guide/started/index.md
+++ docs/guide/started/index.md
@@ -1,5 +1,9 @@
 # 开始
 
+Strve.js 是一个可以将字符串转换为视图的 JS 库。这里的字符串是指模板字符串，所以只需要用 JavaScript 开发视图即可。这里的视图指的是我们平时写的 HTML 页面，也就是视图层。
+
+Strve.js 不仅易于使用，而且可以灵活地拆解不同的代码块。使用模板字符串开发视图，主要是利用原生 JavaScript 的能力，可以更灵活地分离代码块，只关注 JavaScript 文件。
+
 尝试 Strve.js 的方法是在浏览器中打开它，并按照示例学习一些基本用法。
 
 ## ES Modules
@@ -35,7 +39,7 @@
 			function App() {
 				return h`
 						<h1 $key>${state.count}</h1>
-						<button onClick=${add}>Add</button> 
+						<button onClick=${add}>Add</button>
 				`;
 			}
 
@@ -75,7 +79,7 @@
 			function App() {
 				return h`
 						<h1 $key>${state.count}</h1>
-						<button onClick=${add}>Add</button> 
+						<button onClick=${add}>Add</button>
 				`;
 			}
 
